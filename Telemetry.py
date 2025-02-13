ac44d8a5f018170b1d81d5a6b6d4c3eb981dec8b
Kaden Bautista <67088677+Tategami99@users.noreply.github.com>
2025-02-13 10:01:00 -0800

    [Untested] Manual control using controller

--- Telemetry.py
+++ Telemetry.py
@@ -1,20 +1,71 @@
 import asyncio
 from mavsdk import System
+from CustomRylr.py import recvMessage
 
 
 async def run():
     # Init the drone
     drone = System()
     await drone.connect(system_address="udp://:14540")
+    
+    async for state in drone.core.connection_state():
+		if state.is_connected:
+			print("Drone Connected")
+			break
+			
+	print("Arming the drone...")
+	await drone.action.arm()
+	
+	print("Starting manual control...")
+	await drone.manual_control.start_position_control()
 
-    # Start the tasks
-    asyncio.ensure_future(print_battery(drone))
-    asyncio.ensure_future(print_gps_info(drone))
-    asyncio.ensure_future(print_in_air(drone))
-    asyncio.ensure_future(print_position(drone))
+    # Start the tasks, Commented out for now to make testing easier
+    #asyncio.ensure_future(print_battery(drone))
+    #asyncio.ensure_future(print_gps_info(drone))
+    #asyncio.ensure_future(print_in_air(drone))
+    #asyncio.ensure_future(print_position(drone))
+    
+    control_task = asyncrio.create_task(control_drone(drone))
+    try:
+		await asyncio.Event().wait()
+	except KeyboardInterrupt:
+		print("Exiting...")
+		
+	finally:
+		control_task.cancel()
+		await control_task
+		
+		print("Stopping manual control...")
+		await drone.manual_control.stop_position_control()
+		print("Landing...")
+		await drone.action.land()
 
-    while True:
-        await asyncio.sleep(1)
+async def control_drone(drone):
+	while True:
+			try:
+				parts = await recvMessage(reader)
+				data = parts[2]
+				#print("data: " + data)
+				split_data = data.split('/')
+				throttle, yaw, pitch, roll = map(float, split_data)
+				normalized_roll = roll / 1000
+				normalized_pitch = pitch / 1000
+				normalized_yaw = yaw / 1000
+				normalized_throttle = throttle / 1000
+				print(f"Throttle: {normalized_throttle}, Yaw: {normalized_yaw}, Pitch: {normalized_pitch}, Roll: {normalized_roll}")
+					
+				await drone.manual_control.set_manual_control_input(
+					roll, pitch, throttle, yaw
+				)
+			
+			except ValueError:
+				print("Error: Invalid data format, skipping...")	
+			except asyncio.CancelledError:
+				print("Stopping control loop...")
+				break
+					
+			await asyncio.sleep(0.1)
+				
 
 
 async def print_battery(drone):
